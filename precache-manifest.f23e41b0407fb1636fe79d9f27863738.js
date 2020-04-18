4f614f88068220be6e599a0107d815e6b4b6dbbf
jbcastro <josephbeckcastro@gmail.com>
2020-04-18 15:02:42 -0700

    Updates

---
+++ precache-manifest.f23e41b0407fb1636fe79d9f27863738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b7a5c0360156084fb2bdb930d0ac57",
+    "url": "/admin_front/index.html"
+  },
+  {
+    "revision": "8fb886ff9b91852dde12",
+    "url": "/admin_front/static/css/main.9bb0815a.chunk.css"
+  },
+  {
+    "revision": "a918fbd5e6b86f212c18",
+    "url": "/admin_front/static/js/2.5f40dc4f.chunk.js"
+  },
+  {
+    "revision": "5fef2e99b3a9c866132ad042a2a84475",
+    "url": "/admin_front/static/js/2.5f40dc4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb886ff9b91852dde12",
+    "url": "/admin_front/static/js/main.c368fd13.chunk.js"
+  },
+  {
+    "revision": "76fb536b968753ddd039",
+    "url": "/admin_front/static/js/runtime-main.212d3fa9.js"
+  }
+]);
